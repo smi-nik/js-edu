182a761590587952535d30f920d4d110f0c04cc4
Nikolay <n.smirnov.kz@gmail.com>
2019-09-27 16:45:01 +0600

    submit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
+const LEARN_BASICS_HOURS = 500;
+const BE_A_MASTER_HOURS = 800;
+
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+		focus = 'family', 
+		knowsProgramming = true,
+		config = {family: 4}
+	) {
+      const totalHoursNeeded = knowsProgramming ? BE_A_MASTER_HOURS : BE_A_MASTER_HOURS + LEARN_BASICS_HOURS;
+      return Math.ceil(totalHoursNeeded / config[focus]);
+};
